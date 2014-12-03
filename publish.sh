5118f9be1f2acfe84b8c7b5858c1897d2dacedb9
miaoyin <wangyq0616@gmail.com>
2014-12-04 01:19:01 +0800

    博客发布

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+git checkout master
+git add -A
+git commit -m "博客发布"
+cp -r _site/ /tmp/
+git checkout gh-pages
+rm -r ./*
+cp -r /tmp/_site/* ./
+git add -A
+git commit -m "deploy blog"
+git push origin gh-pages
+git checkout master
+echo "发布成功"
